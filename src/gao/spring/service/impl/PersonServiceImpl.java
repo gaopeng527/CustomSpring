5c6c44064d1907a7559c0b245502796bcef018d6
gaopeng527 <1259728004@qq.com>
2016-07-13 21:06:04 +0800

    在服务层调用持久层，注入持久层对象

--- src/gao/spring/service/impl/PersonServiceImpl.java
+++ src/gao/spring/service/impl/PersonServiceImpl.java
@@ -1,9 +1,11 @@
 package gao.spring.service.impl;
 
+import gao.spring.dao.PersonDao;
 import gao.spring.service.PersonService;
 
 public class PersonServiceImpl implements PersonService {
-
+	private PersonDao personDao;
+	
 	public PersonServiceImpl() {
 		System.out.println("PersonServiceImpl初始化");
 	}
@@ -16,12 +18,20 @@ public class PersonServiceImpl implements PersonService {
 	@Override
 	public void save() throws Exception {
 		System.out.println("这是save()方法");
-		
+		personDao.add();
 	}
 	
 	// 可在bean中配置调用
 	public void destory(){
 		System.out.println("关闭资源！");
+	}
+
+	public PersonDao getPersonDao() {
+		return personDao;
+	}
+
+	public void setPersonDao(PersonDao personDao) {
+		this.personDao = personDao;
 	}
 	
 }
